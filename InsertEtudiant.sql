82377e724092ab3bd42c35d86a65696abdc30d6b
BENABDALLAH Yahya <yahya.ben.abdallah@gmail.com>
2020-01-12 19:22:34 +0100

    Question 2 : Enregistrement

---
+++ InsertEtudiant.sql
@@ -0,0 +1,20 @@
+USE [RevisionTp]
+GO
+
+INSERT INTO Etudiant
+           (nom
+           ,prenom
+           ,email
+           ,tel
+           ,login
+           ,password)
+     VALUES
+           ( 'Kader'
+           , 'BENABDALLAH'
+           , 'Qolys@gmail.com'
+           ,01234567
+           ,'varchar(50)'
+           ,'varchar(50)')
+GO
+
+
